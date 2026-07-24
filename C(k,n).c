24d4a74b25bbf5d705bb7c50795099e8deefdfe5
Nguyen Quoc Thanh Vinh <thanhvinhabi132@gmail.com>
2026-07-24 10:21:32 +0700

    Update and rename main.c to C(k,n).c

--- C(k,n).c
+++ C(k,n).c
@@ -1,25 +1,3 @@
-// Số Fibonacci 
-#include <stdio.h>
-
-
-int F(int n) {
-    if ( n == 0 || n == 1){
-        return n;
-    }
-    else {
-        return F(n-1) + F(n-2);
-    }
-}
-
-
-int main() {
-    int n;
-    scanf ("%d", &n);
-    printf ("%d", F(n));
-    return 0;
-}
-
-
 // CODE TÍNH TỔ HỢP CHẬP K CỦA N BẰNG HÀM ĐỆ QUY TRONG LẬP TRÌNH C
 #include <stdio.h>
 
